23e1e68e508965c0ef094a520c2057dfcd615f04
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2024-01-09 18:49:10 -0300

    how to use style and themes

--- app/src/main/java/com/example/jetpackcomposestudies/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposestudies/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackcomposestudies
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposestudies.ui.theme.JetpackComposeStudiesTheme
@@ -105,6 +107,13 @@ private fun Greetings(
     }
 }
 
+// preview no modo dark
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 private fun GreetingsPreview() {
@@ -140,7 +149,9 @@ private fun Greeting(name: String) {
         ) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
 
             ElevatedButton(
