d27856e816e4d7a7bef50a519cb274af14fd132c
Paula Eduarda Justino <rpaulaeduarda@gmail.com>
2024-01-09 16:34:54 -0300

    how to use states

--- app/src/main/java/com/example/jetpackcomposestudies/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposestudies/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,20 +45,28 @@ private fun MyApp(
 
 @Composable
 private fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val bottomPadding = if (expanded.value) 72.dp else 24.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp)) {
+        Row(modifier = Modifier.padding(
+            start = 24.dp,
+            end = 24.dp,
+            top = 24.dp,
+            bottom = bottomPadding
+        )) {
             Column(modifier = Modifier.weight(1f)) {
                 Text(text = "Hello,")
                 Text(text = name)
             }
-            
+
             ElevatedButton(
-                onClick = { /*TODO*/ }
+                onClick = { expanded.value = !expanded.value },
             ) {
-                Text(text = "Show more")
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
